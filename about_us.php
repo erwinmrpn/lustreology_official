0925ad0b85c057b9aaa21ae5e226feb1edcc29f7
erwinmrpn <erwinkho2@gmail.com>
2025-12-06 20:51:12 +0700

    Merapikan struktur code

--- about_us.php
+++ about_us.php
@@ -54,32 +54,32 @@
                             </div>
                             <!-- Experience Card 2-->
                             <div class="card shadow border-0 rounded-4 mb-5">
-    <div class="card-body p-5">
-        <div class="row align-items-center gx-5">
+                                <div class="card-body p-5">
+                                     <div class="row align-items-center gx-5">
             
-            <div class="col text-center text-lg-start mb-4 mb-lg-0">
-                <div class="bg-light p-4 rounded-4">
-                    <div class="text-primary fw-bolder mb-2">AKREDITASI</div>
-                    <div class="small fw-bolder">Sistem Informasi</div>
-                    <div class="small fw-bolder">E-Commerce</div>
-                    <div class="small fw-bolder">Game Technology</div>
-                    <div class="small fw-bolder">AKSI</div>
-                </div>
-            </div>
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-primary fw-bolder mb-2">AKREDITASI</div>
+                                                <div class="small fw-bolder">Sistem Informasi</div>
+                                                <div class="small fw-bolder">E-Commerce</div>
+                                                <div class="small fw-bolder">Game Technology</div>
+                                                <div class="small fw-bolder">AKSI</div>
+                                            </div>
+                                        </div>
             
-            <div class="col-lg-8">
-                <div class="image-box-fill rounded-4 shadow-sm">
-                    <img 
-                        src="assets/sertifikat_akreditasi.png" 
-                        alt="Sertifikat Akreditasi" 
-                        class="img-fluid"
-                    />
-                </div>
-            </div>
+                                        <div class="col-lg-8">
+                                            <div class="image-box-fill rounded-4 shadow-sm">
+                                                <img 
+                                                    src="assets/sertifikat_akreditasi.png" 
+                                                    alt="Sertifikat Akreditasi" 
+                                                    class="img-fluid"
+                                                />
+                                            </div>
+                                            </div>
             
-        </div>
-    </div>
-</div>
+                                </div>
+                            </div>
+                        </div>
                         </section>
                         <!-- Education Section-->
                         <section>
