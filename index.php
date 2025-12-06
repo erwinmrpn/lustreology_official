b7104d54f6b8c5e208496b18bb0c3d31ff4657a5
erwinmrpn <erwinkho2@gmail.com>
2025-12-06 19:51:54 +0700

    merubah nama file resume.php  menjadi abut_us.php dan project.php menjadi blog.php

--- index.php
+++ index.php
@@ -34,8 +34,8 @@
                                 <div class="fs-3 fw-light text-muted">SIEGA adalah</div>
                                 <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Gaya hidup, Semangat, dan Pengalaman Baru</span></h1>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
-                                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume.php">About Us</a>
-                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="projects.php">Blog</a>
+                                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="about_us.php">About Us</a>
+                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="blog.php">Blog</a>
                                 </div>
                             </div>
                         </div>
