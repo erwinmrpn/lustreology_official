b2d6b23fd9d62e74e34c1e6592a25dd7c6119f8e
erwinmrpn <erwinkho2@gmail.com>
2025-12-16 23:58:17 +0700

    Editing Footer

--- about_us.php
+++ about_us.php
@@ -145,7 +145,7 @@ $kategori  = mysqli_query($conn, "
     <!-- Footer-->
     <footer class="bg-white py-4 mt-auto">
         <div class="container px-5">
-            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+            <div class="row align-items-center justify-content-center">
                 <div class="col-auto">
                     <div class="small m-0">Copyright &copy; erwinmrpn</div>
                 </div>
--- blog.php
+++ blog.php
@@ -115,9 +115,10 @@ include "admin/proses/koneksi.php";
             </div>
         </section>
     </main>
+    <!-- footer -->
     <footer class="bg-white py-4 mt-auto">
         <div class="container px-5">
-            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+            <div class="row align-items-center justify-content-center">
                 <div class="col-auto">
                     <div class="small m-0">Copyright &copy; erwinmrpn</div>
                 </div>
